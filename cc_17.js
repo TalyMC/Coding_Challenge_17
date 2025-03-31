da3e495493cac04bf5d92b8723422744075dc610
Natalie Mauricio-Cruz <mauriciocruzn@usf.edu>
2025-03-30 22:30:02 -0400

    Task 2- Extended VIPCustomer class

--- cc_17.js
+++ cc_17.js
@@ -32,3 +32,29 @@ customer1.addPurchase(200);
 // Console Log
 console.log(customer1.details());
 console.log(customer2.details());
+
+//Task 2
+class SalesRep {
+    constructor(name) {
+        this.name = name;
+        this.client = [];
+    }
+    addClient(customer) {
+        this.client.push(customer);
+    }
+    getClientTotal(name) {
+        const client = this.client.find(client => client.name === name);
+        return client ? client.totalSpent() : 0;
+    }
+}
+
+// SalesRep
+const salesRep1 = new SalesRep("Alexander Hamilton");
+
+// Assigning Customers
+salesRep1.addClient(customer1);
+salesRep1.addClient(customer2);
+
+// Console Log
+console.log(`Sales rep: ${salesRep1.name}, Client: ${customer1.name}, Total spent: $${salesRep1.getClientTotal(customer1.name)}`);
+console.log(`Sales rep: ${salesRep1.name}, Client: ${customer2.name}, Total spent: $${salesRep1.getClientTotal(customer2.name)}`);
